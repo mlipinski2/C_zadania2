6fb1df71730fba7819ff1568da7f88e98d92edff
Michał Lipiński <mlipinski2@sigma.ug.edu.pl>
2013-12-06 22:41:16 +0100

    indent 1

--- Func_3.c
+++ Func_3.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
- int main() {
- int liczba, i, suma;
-  printf("Liczby doskonale pomiedzy 1 a 10000: \n");
-  for (liczba = 1; liczba <= 10000; liczba++) 
-   {
-    
-   i = 1;
-   suma = 0;
-   while (i < liczba) 
-    
-     {
-     if (liczba%i == 0)
-      suma = suma + i;
-      i++;
-    
-     }
-   if (suma == liczba)
-    printf("%d \n", liczba);
-   }
-   
- return 0;
-}
+int
+main ()
+{
+  int liczba, i, suma;
+  printf ("Liczby doskonale pomiedzy 1 a 10000: \n");
+  for (liczba = 1; liczba <= 10000; liczba++)
+    {
 
+      i = 1;
+      suma = 0;
+      while (i < liczba)
+
+	{
+	  if (liczba % i == 0)
+	    suma = suma + i;
+	  i++;
+
+	}
+      if (suma == liczba)
+	printf ("%d \n", liczba);
+    }
+
+  return 0;
+}
