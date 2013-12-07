0e5b0b0b995b8f15d1ed59f7519daa6958872dfa
Michał Lipiński <mlipinski2@sigma.ug.edu.pl>
2013-12-07 15:49:51 +0100

    zadanie 2

---
+++ 7_funkcje/zadanie2_after_indent.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+long silnia (int);
+
+int
+main ()
+{
+  int liczba_naturalna;
+  long silnia = 1;
+
+  printf ("Podaj liczbę naturalną z której obliczymy silnię:\n");
+  scanf ("%d", &liczba_naturalna);
+
+  if (liczba_naturalna < 0)
+
+    printf ("Liczba musi być wieksza od zera.\n");
+  else
+    {
+
+      printf ("%d! = %ld\n", liczba_naturalna, silnia (liczba_naturalna));
+
+    }
+  return 0;
+
+}
+
+long
+silnia (int x)
+{
+  int a;
+  long out = 1;
+
+  for (a = 1; a <= x; a++)
+    out = out * a;
+
+  return result;
+}
