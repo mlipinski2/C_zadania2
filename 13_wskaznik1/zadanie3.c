58804a483b1a016a034ffdc51fbcca9f41dc731c
Michał Lipiński <mlipinski2@p109-14.labpk.inf.ug.edu.pl>
2013-12-15 12:22:44 +0100

    wskazniki

---
+++ 13_wskaznik1/zadanie3.c
@@ -0,0 +1,16 @@
+void podnies_do_kwadratu(int *n) {
+  
+}
+
+void wczytaj_liczbe(int *n) {
+  printf("Wpisz liczbę naturalną: ");
+  scanf("%d", &n);
+}
+
+int main() {
+   int liczba;
+   wczytaj_liczbe(&liczba);
+   podnies_do_kwadratu(&liczba);
+   printf("Kwadrtat wczytanej liczby to %d\n", liczba);
+   return 0;
+}
