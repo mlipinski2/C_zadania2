cfd4f472b1ad49d12e61eb775645721851a25367
Michał Lipiński <mlipinski2@p109-14.labpk.inf.ug.edu.pl>
2013-12-15 09:47:02 +0100

    rekurencja

--- 11_rekurencja/zadanie1.c
+++ 11_rekurencja/zadanie1.c
@@ -2,26 +2,26 @@
 
 int main(void){
 
-    int a, b;
+    int a, b, sil;
     long long int out;
     printf("podaj liczbe której potęgę bedziemy obliczać: ");
     scanf("%d", &a);
     printf("podaj wartość potęgi: ");
     scanf("%d", &b);
-    out = silnia(a,b);  
+    out = sil(a,b);  
     printf("wynik z %d do potęgi %d to: %lld \n", a, b, out);
     return 0;
 }
 
-int silnia (int liczba)
+int b (int a)
 {
   int sil;
-  if (liczba<0) {
+  if (a<0) {
      return 0;
      } 
-  if (liczba==0 || liczba==1) {
+  if (a==0 || a==1) {
      return 1;
      }
-  sil = liczba*silnia(liczba-1);
+  sil = a*b(a-1);
   return sil;
 }
