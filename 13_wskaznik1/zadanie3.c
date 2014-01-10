12fb2ebe76bb750b541cb5f2bbb15ea13f123383
mlipinski2 <mlipinski2@sigma.ug.edu.pl>
2014-01-10 20:25:43 +0100

    Update zadanie3.c
    zad3

--- 13_wskaznik1/zadanie3.c
+++ 13_wskaznik1/zadanie3.c
@@ -14,3 +14,28 @@ int main() {
    printf("Kwadrtat wczytanej liczby to %d\n", liczba);
    return 0;
 }
+
+/*
+#include<stdio.h>
+
+void podnies_do_kwadratu(int *n) {
+    int b;
+    b = *n;
+    *n = (b*b);
+}
+
+void wczytaj_liczbe(int *n) {
+    printf("Wpisz liczbę naturalną: ");
+    scanf("%d", n); 
+}
+
+int main() {
+    int liczba;
+    int *n;
+    n = &liczba;
+    wczytaj_liczbe(&liczba);
+    podnies_do_kwadratu(&liczba);
+    printf("Kwadrtat wczytanej liczby to %d\n", liczba);
+    return 0;
+}
+*/
